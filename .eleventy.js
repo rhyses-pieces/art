c7f5d98b3f6518a74a5bc5e6f24d1b6c930a6c0c
rhys <lev.k.kim@gmail.com>
2022-12-09 13:17:38 -0500

    remove packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,12 @@
 const htmlmin = require("html-minifier");
-const { DateTime } = require('luxon');
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const navigation = require('@11ty/eleventy-navigation');
-const vite = require("@11ty/eleventy-plugin-vite");
 const imageShortcode = require('./src/_shortcodes/image');
 const renderShortcode = require('./src/_shortcodes/render');
 const dateFilter = require('./src/_filters/date');
 const renderHtmlFilter = require('./src/_filters/renderHtml');
 
 module.exports = function(eleventyConfig) {
-
-  let copyOptions = {
-    debug: true,
-  }
 
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
@@ -21,7 +15,6 @@ module.exports = function(eleventyConfig) {
   // Plugins
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(navigation);
-  eleventyConfig.addPlugin(vite);
 
   // Shortcodes
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
